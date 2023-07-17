bea2929b7e1a2ec84fe468e9017b8164abf9fe9b
pmalen327 <malenpreston@gmail.com>
2023-07-17 10:06:44 -0600

    fixed exception

--- driver.py
+++ driver.py
@@ -13,6 +13,9 @@ t = np.linspace(0, 100, 11)
 u = [np.random.randint(-20, 20) for i in t]
 v = [np.random.randint(-20, 20) for i in t]
 
+
+
+
 # computes W_1 for two probability measures u and v
 # discretely, we need pairs (t, u(t)) and (t, v(t))
 # will normalize u and v if sum neq 1
@@ -20,6 +23,7 @@ v = [np.random.randint(-20, 20) for i in t]
 # args are ordered like (u(t), v(t), t, t) where t is the Dirac mass centered
 # at t
 w1 = wasserstein_distance(u, v, t, t)
+
 
 # if inf(f) is negative, we send f - inf(f) to the new value
 # i.e. shifting everything up by the value of the min
@@ -40,15 +44,16 @@ def pos_shift(f):
             if i < 0:
                 break
             else: 
-                return None # how to throw exception here??
+                raise Exception('f has no negative values')
+            
         pos_f = [abs(val) for val in f]
         return pos_f
+    
     except:
         print('f has no negative values')
 
 
 #TODO
-# figure out line 49, small technicality
 # wrap in a main method maybe?? not sure 
     
 

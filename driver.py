0f45de71190eb826ba21e6cd344cadcbdd3f1fb1
pmalen327 <malenpreston@gmail.com>
2023-08-05 11:48:09 -0600

    removed posShift, misunderstanding on my end

--- driver.py
+++ driver.py
@@ -41,23 +41,6 @@ def vert_shift(f):
         print('f has no negative values')
 
 
-# takes the L1 norm of every value in f
-# man this is cursed, will definitely need to be optimized later
-def pos_shift(f):
-    try:
-        for i in f:
-            if i < 0:
-                break
-            else: 
-                raise Exception('f has no negative values')
-            
-        pos_f = [abs(val) for val in f]
-        return pos_f
-    
-    except:
-        print('f has no negative values')
-
-
 test_matrix = []
 for i in range(n):
     test_matrix.append([np.random.randint(-20, 20) for i in t])

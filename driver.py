cfde0e3a758fa39221d1e277f6801a871382cc77
Preston Malen <90215402+pmalen327@users.noreply.github.com>
2023-08-14 06:54:41 -0600

    Update driver.py

--- driver.py
+++ driver.py
@@ -1,70 +1,84 @@
 # Preston Malen
 # July 2023
 
+import pickle
+import mpu
 import numpy as np
 import pandas as pd
 import gudhi as gd
 import sklearn
+
 from sklearn.preprocessing import normalize
 from sklearn.metrics import pairwise_distances
 from scipy.stats import wasserstein_distance
 
 from random import randrange
-np.random.seed(0)
+
+
+def main(data):
+
+    # computes W_1 for two probability measures u and v
+    # discretely, we need pairs (t, u(t)) and (t, v(t))
+    # will normalize u and v if sum neq 1
+
+    # args are ordered like (u(t), v(t), t, t) where t is the Dirac mass centered
+    # at t
+    def w1(u, v):   # the distance matrix metric arg. can only take two inputs
+        return wasserstein_distance(u, v, t, t)
+
+
+    # if inf(f) is negative, we send f - inf(f) to the new value
+    # i.e. shifting everything up by the value of the min
+    def vert_shift(f):
+        try:
+            if min(f) < 0:
+                vert_f = [i + abs(min(f)) for i in f]
+            return vert_f
+        
+        except:
+            print('f has no negative values')
+
+
+    ds = sklearn.metrics.pairwise_distances(data, metric = w1)
+
+    # will normalizing affect the w1 distance??
+    skeleton = gd.RipsComplex(
+        distance_matrix = ds,
+        max_edge_length = 8
+    )
+
+    return skeleton.create_simplex_tree(max_dimension = 5)
+   
+
 
 n = 100
-# here, the mesh t needs to be previously computed
+# # here, the mesh t needs to be previously computed
 t = np.linspace(0, 100, 11)
-u = [np.random.randint(-20, 20) for i in t]
-v = [np.random.randint(-20, 20) for i in t]
-
-
-# computes W_1 for two probability measures u and v
-# discretely, we need pairs (t, u(t)) and (t, v(t))
-# will normalize u and v if sum neq 1
-
-# args are ordered like (u(t), v(t), t, t) where t is the Dirac mass centered
-# at t
-def w1(u, v):   # the distance matrix metric arg. can only take two inputs
-    return wasserstein_distance(u, v, t, t)
-
-
-# if inf(f) is negative, we send f - inf(f) to the new value
-# i.e. shifting everything up by the value of the min
-def vert_shift(f):
-    try:
-        if min(f) < 0:
-            vert_f = [i + abs(min(f)) for i in f]
-        return vert_f
-    
-    except:
-        print('f has no negative values')
-
+# u = [np.random.randint(-20, 20) for i in t]
+# v = [np.random.randint(-20, 20) for i in t]
 
 test_matrix = []
 for i in range(n):
     test_matrix.append([np.random.randint(-20, 20) for i in t])
 
 test_matrix = np.array(test_matrix)
-    
-ds = sklearn.metrics.pairwise_distances(test_matrix, metric = w1)
 
-# will normalizing affect the w1 distance??
-skeleton = gd.RipsComplex(
-    distance_matrix = ds,
-    max_edge_length = 8
-)
 
-simplex_tree = skeleton.create_simplex_tree(max_dimension = 5)
-print(simplex_tree.num_simplices())
+
+if __name__ == "__main__":
+    simplex_tree = main(test_matrix)
+    fileObj = open('simplex_tree.obj', 'wb')
+    pickle.dump(simplex_tree, fileObj)
+    fileObj.close()
+
+
 
 #TODO
-# implement gudhi W_p metric
-# pickle the returnd simplex tree object
+# pickle the returned simplex tree object
+    # fix pickle
 # filter max/min dimensions for persistence barcode/line graph
 # play with max_edge_length
 # normalize data
 # ACCOUNT FOR NEGATIVE VALUES IN TIME SERIES >> need to write two functions to
     # check for negative values and adjust accordingly
-# wrap in main method maybe?
 # fix noise on n-sphere demo (lives in ~\RiemannTDA)

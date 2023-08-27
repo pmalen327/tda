c95b76f99db3a815f720d0ef600668f2b908b8b6
Preston Malen <90215402+pmalen327@users.noreply.github.com>
2023-08-27 12:42:52 -0600

    Update timeSeries.py

--- timeSeries.py
+++ timeSeries.py
@@ -16,18 +16,13 @@ df = pd.read_csv('stocks.csv')
 df = df.drop('Date', axis=1)
 df = df.drop('Volume', axis=1)
 data = pd.DataFrame(df).to_numpy()
-print(data.shape)
 
 # I have something backwards, I think I know but will need to sketch it out
 if __name__ == '__main__':
-    t = np.linspace(0, data.shape[0])
-    simplex_tree = main(data, t, max_edge_length=2, max_dimension=3, regularization='none')
+    t = [i+1 for i in range(data.shape[1])]
+    simplex_tree = main(data, t, max_edge_length=3, max_dimension=3, regularization='none')
     print(simplex_tree.num_simplices())
 
-
-
-
-
-
-
-
+    fileObj = open('timeSeries_simplex_tree.obj', 'wb')
+    pickle.dump(simplex_tree, fileObj)
+    fileObj.close()
